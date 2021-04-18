e16db44ee9fdee64a5fed8b96bf0999327af6b32
saqla <whitecross8fortune8fatedress@gmail.com>
2021-04-18 12:24:25 +0900

    💄 Swiper Component の配置調整

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import Head from 'next/head'
 import { Header } from '../components/Header'
+import { SwiperComponent } from '../components/swiper'
 
 export default function Home() {
   return (
-    <div className="container m-auto">
+    <div className='container m-auto'>
       <Head>
         <title>M-H-D</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <Header />
-      <main>
-
+      <main className='twoColumn-container'>
+        <SwiperComponent />
+        <div className="bg-red-500 w-2/6">aside contents</div>
       </main>
-      <footer>
-      </footer>
+      <footer></footer>
     </div>
   )
 }
