cc4db2b77dc4eaefa5930787c9a20dbeaf16ac5b
saqla <whitecross8fortune8fatedress@gmail.com>
2021-04-18 14:23:00 +0900

    ♻️ グローバルメニューをmapで処理とデータに分割

--- src/components/GlobalMenu.jsx
+++ src/components/GlobalMenu.jsx
@@ -1,26 +1,35 @@
 import React from 'react'
 import Link from 'next/link'
 
+const MENUITEMS = [
+  {
+    href: '/',
+    title: 'トップページ',
+  },
+  {
+    href: 'https://jp.finalfantasyxiv.com/lodestone/',
+    title: 'トピックス',
+  },
+  // {
+  //   href: '/',
+  //   title: '設定参考',
+  // },
+]
+
 export const GlobalMenu = () => {
   return (
-    <div className="hidden md:block">
+    <div className='hidden md:block'>
       {/* グローバルメニュー */}
-      <ul className="flex-around bg-black text-white text-2xl py-1">
-        <li className="y-border">
-          <Link href="/">
-            <a className="mx-3 cursor-pointer">トップページ</a>
-          </Link>
-        </li>
-        <li className="y-border">
-          <Link href="https://jp.finalfantasyxiv.com/lodestone/">
-            <a className="mx-3 cursor-pointer">トピックス</a>
-          </Link>
-        </li>
-        {/* <li className="y-border">
-          <Link href="">
-            <a className="mx-3 cursor-pointer">設定参考</a>
-          </Link>
-        </li> */}
+      <ul className='flex-around bg-black text-white text-2xl py-1'>
+        {MENUITEMS.map((item) => {
+          return (
+            <li key={item.href} className='y-border'>
+              <Link href={item.href}>
+                <a className='mx-3 cursor-pointer'>{item.title}</a>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
