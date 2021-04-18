a5913f518a7c9162e1e37cdc0babb3dfc9552efc
saqla <whitecross8fortune8fatedress@gmail.com>
2021-04-18 10:55:17 +0900

    💄 トップへのリンクを修正

--- src/components/GlobalMenu.jsx
+++ src/components/GlobalMenu.jsx
@@ -7,18 +7,18 @@ export const GlobalMenu = () => {
       {/* グローバルメニュー */}
       <ul className="flex-around bg-black text-white text-2xl py-1 lg:text-4xl lg:py-3">
         <li className="y-border">
-          <Link href="">
-            <p className="mx-3">トップページ</p>
+          <Link href="https://m-h-d.family/">
+            <p className="mx-3 cursor-pointer">トップページ</p>
           </Link>
         </li>
         <li className="y-border">
           <Link href="https://jp.finalfantasyxiv.com/lodestone/">
-            <p className="mx-3">トピックス</p>
+            <p className="mx-3 cursor-pointer">トピックス</p>
           </Link>
         </li>
         <li className="y-border">
           <Link href="">
-            <p className="mx-3">設定参考</p>
+            <p className="mx-3 cursor-pointer">設定参考</p>
           </Link>
         </li>
       </ul>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
     <>
       {/* タイトル */}
       <div className="flex-between my-2">
-        <Link href="https://m-h-d-v-next.vercel.app/">
+        <Link href="https://m-h-d.family/">
           <h1 className="text-2xl italic mx-5 md:text-4xl md:mx-10 lg:text-6xl">M-<span className="text-red-600">H</span>-D</h1>
         </Link>
         <p className="text-sm mx-2 md:text-xl md:mx-5 lg:text-3xl">Final Fantasy XIV 〜FCウェブページ〜</p>
